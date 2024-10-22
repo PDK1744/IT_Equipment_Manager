f52013a8ec098198ed76de3892ae634edb85bdde
Kobe <kbonn43@wgu.edu>
2024-10-22 16:25:18 -0500

    Fleshed out CRUD operations for PC Inventory

--- src/pcInventoryRepository.js
+++ src/pcInventoryRepository.js
@@ -1,16 +1,108 @@
-const client = require('../db');
+const client = require('./db');
 
-async function getAllPCInventory() {
-    const res = await client.query('SELECT * FROM pc_inventory');
-    return res.rows;
+// Function to get all PCs
+async function getAllPCs() {
+    try {
+        const result = await client.query('SELECT * FROM pc_inventory');
+        return result.rows;
+    } catch (err) {
+        console.error('Error fetching PCs:', err);
+        throw err;
+    }
 }
 
-async function deletePCInventoryItem(itemId) {
-    const res = await client.query('DELETE FROM pc_inventory WHERE id = $1', [itemId]);
-    return res.rowCount;
+// Function to add a new PC
+async function addPC(pcData) {
+    const { pc_number, ease_number, model, asset_tag, service_tag, warranty_expiration, location, branch, notes } = pcData;
+    try {
+        const result = await client.query(
+    `INSERT INTO pc_inventory (
+        pc_number, 
+        ease_number, 
+        model, 
+        asset_tag, 
+        service_tag, 
+        warranty_expiration, 
+        location, 
+        branch, 
+        notes
+    ) VALUES (
+        $1, $2, $3, $4, $5, $6, $7, $8, $9
+    ) RETURNING *`,
+    [
+        pc_number, 
+        ease_number, 
+        model, 
+        asset_tag, 
+        service_tag, 
+        warranty_expiration, 
+        location, 
+        branch, 
+        notes
+    ]
+);
+        return result.rows[0];
+    } catch (err) {
+        console.error('Error adding PC:', err);
+        throw err;
+    }
+}
+
+// Function to update a PC
+async function updatePC(id, pcData) {
+    const { pc_number, ease_number, model, asset_tag, service_tag, warranty_expiration, location, branch, notes } = pcData;
+    try {
+        const result = await client.query(
+            `
+            UPDATE pc_inventory 
+            SET 
+                pc_number = $1, 
+                ease_number = $2, 
+                model = $3, 
+                asset_tag = $4, 
+                service_tag = $5, 
+                warranty_expiration = $6, 
+                location = $7, 
+                branch = $8, 
+                notes = $9 
+            WHERE 
+                id = $10 
+            RETURNING *
+            `,
+            [
+                pc_number, 
+                ease_number, 
+                model, 
+                asset_tag, 
+                service_tag, 
+                warranty_expiration, 
+                location, 
+                branch, 
+                notes,
+                id 
+            ]
+        );
+        return result.rows[0];
+    } catch (err) {
+        console.error('Error updating PC:', err);
+        throw err;
+    }
+}
+
+// Function to delete a PC
+async function deletePC(id) {
+    try {
+        const result = await client.query('DELETE FROM pc_inventory WHERE id = $1 RETURNING *', [id]);
+        return result.rows[0];
+    } catch (err) {
+        console.error('Error deleting PC:', err);
+        throw err;
+    }
 }
 
 module.exports = {
-    getAllPCInventory,
-    deletePCInventoryItem,
+    getAllPCs,
+    addPC,
+    updatePC,
+    deletePC,
 };
